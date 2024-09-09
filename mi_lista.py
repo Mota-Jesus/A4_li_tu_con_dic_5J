eb3b122570ea3bbdb4fc1476e6767445a328a962
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-09-09 10:12:31 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,28 @@
+
+print("Ejemplo de listas")
+arcoiris=["verde","azul","morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print(longitud)
+print("Elementos que contien la lista arcoiris:",longitud)
+print(f"Elementos que contien la lista arcoiris v2: {longitud}")
+print("accediendo a un elemento")
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print(f"El primer color del arcoiris es:",arcoiris[0])
+print("agregar a un elemtento de la lista")
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar los elementos de la lista ciclo for")
+for mota in arcoiris:
+    print(mota)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ mis_tuplas.py
@@ -0,0 +1,8 @@
+print("Ejemplos de tuplas")
+canciones=("Parte De","PABLO","LINDO")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "Fascination"
+x = tuple(y)
+print(x)
