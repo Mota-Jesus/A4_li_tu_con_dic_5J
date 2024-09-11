56f1e87ed6cbb824814a5d195c36b4abf016147b
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-09-11 07:37:28 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de conjuntos set")
+tipodeportes={"Fubo","Basket","Tochito🤙" }
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+for Mota in tipodeportes:
+    print(Mota)
