a8155f0ad4a7101ef7960a287ac0751ec03bfd62
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-09-12 11:07:06 -0600

    Add files via upload

---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+print("Marca:",thisdict["brand"])
+print("Modelo:",thisdict["model"])
+print("Año:",thisdict["year"])
